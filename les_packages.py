9f679514342dd719585fc9bffbfc6917ffe4f2a3
TCHETCHE Levi emmanuel <108012413+LEVISSTRAUGHT42@users.noreply.github.com>
2023-06-30 22:13:13 +0200

    Create les_packages.py

---
+++ les_packages.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+from sklearn.datasets import make_blobs#importer les données,
+from sklearn.cluster import KMeans
